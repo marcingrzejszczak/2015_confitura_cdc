07677a08d5c6987cbc8739897e15223d07c02509
Jakub Kubrynski <jkubrynski@gmail.com>
2015-01-29 17:47:46 +0100

    Added server side implementation

--- fraudDetectionService/src/main/java/com/blogspot/toomuchcoding/frauddetection/FraudDetectionController.java
+++ fraudDetectionService/src/main/java/com/blogspot/toomuchcoding/frauddetection/FraudDetectionController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
+import static com.blogspot.toomuchcoding.frauddetection.model.FraudCheckStatus.FRAUD;
 import static com.blogspot.toomuchcoding.frauddetection.model.FraudCheckStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
@@ -14,6 +17,8 @@ public class FraudDetectionController {
 
 	private static final String FRAUD_SERVICE_JSON_VERSION_1 = "application/vnd.fraud.v1+json";
 	private static final String NO_REASON = null;
+	private static final String AMOUNT_TOO_HIGH = "Amount too high";
+	private static final BigDecimal MAX_AMOUNT = new BigDecimal("5000");
 
 	@RequestMapping(
 			value = "/fraudcheck",
@@ -21,7 +26,14 @@ public class FraudDetectionController {
 			consumes = FRAUD_SERVICE_JSON_VERSION_1,
 			produces = FRAUD_SERVICE_JSON_VERSION_1)
 	public FraudCheckResult fraudCheck(@RequestBody FraudCheck fraudCheck) {
+		if (amountGreaterThanThreshold(fraudCheck)) {
+			return new FraudCheckResult(FRAUD, AMOUNT_TOO_HIGH);
+		}
 		return new FraudCheckResult(OK, NO_REASON);
+	}
+
+	private boolean amountGreaterThanThreshold(FraudCheck fraudCheck) {
+		return MAX_AMOUNT.compareTo(fraudCheck.getLoanAmount()) < 0;
 	}
 
 }
