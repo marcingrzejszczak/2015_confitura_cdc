1c9eabde9553e71cdd67a0a84b222b0cc7c906d9
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-07-02 21:14:53 +0200

    Fixed WireMock task and DSL

--- fraudDetectionServer/mappings/fraudDetectionServer/shouldMarkClientAsNotFraud.groovy
+++ fraudDetectionServer/mappings/fraudDetectionServer/shouldMarkClientAsNotFraud.groovy
@@ -1,30 +1,21 @@
 io.codearte.accurest.dsl.GroovyDsl.make {
                 request {
-                method """PUT"""
-                url """/fraudcheck"""
+                method "PUT"
+                url "/fraudcheck"
                 body('''
                     {
                     "clientPesel":"1234567890",
                     "loanAmount":123.123}
                 '''
                 )
-                headers {
-                    header("""Content-Type""", """application/vnd.fraud.v1+json""")
-
-                }
-                
+                headers { header("Content-Type", "application/vnd.fraud.v1+json") }
             }
             response {
                 status 200
                 body( """{
-    "fraudCheckStatus": "OK",
-    "rejectionReason": null
-}""")
-                headers {
-                     header('Content-Type': 'application/vnd.fraud.v1+json')
-
-                    }
-                
+                        "fraudCheckStatus": "OK",
+                        "rejectionReason": null
+                    }""")
+                headers { header('Content-Type': 'application/vnd.fraud.v1+json') }
             }
-        
 }
