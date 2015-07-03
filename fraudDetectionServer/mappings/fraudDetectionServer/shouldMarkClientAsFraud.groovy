7f109ecccca094d9636e9d1c13c59913e4b4686b
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-07-03 21:13:19 +0200

    Refactored code

--- fraudDetectionServer/mappings/fraudDetectionServer/shouldMarkClientAsFraud.groovy
+++ fraudDetectionServer/mappings/fraudDetectionServer/shouldMarkClientAsFraud.groovy
@@ -1,30 +1,21 @@
 io.codearte.accurest.dsl.GroovyDsl.make {
                 request {
-                method """PUT"""
-                url """/fraudcheck"""
+                method "PUT"
+                url "/fraudcheck"
                 body('''
                     {
                     "clientPesel":"1234567890",
                     "loanAmount":99999}
                 '''
                 )
-                headers {
-                    header("""Content-Type""", """application/vnd.fraud.v1+json""")
-
-                }
-                
+                headers { header("Content-Type", "application/vnd.fraud.v1+json") }
             }
             response {
                 status 200
                 body( """{
-    "fraudCheckStatus": "FRAUD",
-    "rejectionReason": "Amount too high"
-}""")
-                headers {
-                     header('Content-Type': 'application/vnd.fraud.v1+json')
-
-                    }
-                
+                        "fraudCheckStatus": "FRAUD",
+                        "rejectionReason": "Amount too high"
+                    }""")
+                headers { header('Content-Type': 'application/vnd.fraud.v1+json') }
             }
-        
 }
